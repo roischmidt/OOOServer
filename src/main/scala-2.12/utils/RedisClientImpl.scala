2223dd84a0b7f87352468db0846a7cdc219d00f8
rois <roi.schmidt77@gmail.com>
2017-02-12 10:59:03 +0200

    use redis async - redis pool

--- src/main/scala-2.12/utils/RedisClientImpl.scala
+++ src/main/scala-2.12/utils/RedisClientImpl.scala
@@ -4,13 +4,15 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
-import com.redis.RedisClient
+import com.redis.{RedisClient, RedisClientPool}
 import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
-import service.LoginService.getClass
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.Failure
+
 /**
   * Created by rois on 10/02/2017.
   */
@@ -27,39 +29,91 @@ object RedisClientImpl {
     
     // Redis client setup
     val config = ConfigFactory.load()
-    var client  = new RedisClient(config.getString("redis.interface"), config.getInt("redis.port"))
+    var clients = new RedisClientPool(config.getString("redis.interface"), config.getInt("redis.port"))
     
-    def init : Boolean = {
+    def asyncRedis[T](body: RedisClient => T): Future[T] =
+        Future {
+            clients.withClient {
+                client => {
+                    client.setConfig("notify-keyspace-events", "KEx")
+                    body(client)
+                }
+            }
+        }.andThen {
+            case Failure(e) => logger.error("redis request failed", e)
+            case ignore =>
+        }
+    
+    
+    def init: Future[Boolean] = {
         logger.info("pinging redis server")
-        client.ping.exists {
-            case res =>
-                logger.info(s"got pong response from redis: $res")
-                true
+        asyncRedis {
+            _.ping.exists {
+                case res =>
+                    logger.info(s"got pong response from redis: $res")
+                    true
+            }
         }
     }
     
+    
     // clear all DB
-    def clearAll() : Boolean = {
-        client.flushdb
+    def clearAll(): Future[Boolean] = {
+        asyncRedis {
+            _.flushdb
+        }
     }
-    
+
     // store CacheData object
-    def store(key: String, value: String): Boolean =
+    def store(key: String, value: String): Future[Boolean] =
         storeEx(key, expired, value)
-    
+
     // store CacheData object with provided expiration
-    def storeEx(key: String, expire: Int, value: String): Boolean =
-        client.setex(key, expire, value)
-    
+    private def storeEx(key: String, expire: Int, value: String): Future[Boolean] =
+        asyncRedis {
+            _.setex(key, expire, value)
+        }
+
     // renew expiration for a specific key
-    def resume(key: String, expiration: Int = expired): Boolean =
-        client.expire(key, expiration)
-    
+    def resume(key: String, expiration: Int = expired): Future[Boolean] =
+        asyncRedis {
+            _.expire(key, expiration)
+        }
+
     // check if key exists
-    def exists(key: String): Boolean =
-        client.exists(key)
-    
+    def exists(key: String): Future[Boolean] =
+        asyncRedis {
+            _.exists(key)
+        }
+
     // remove key
-    def remove(key: String): Boolean =
-        client.del(key).exists(_ > 0)
+    def remove(key: String): Future[Boolean] =
+        asyncRedis {
+            _.del(key).exists(_ > 0)
+        }
+
+    // add to set
+    def addToSet(key: String, value: String): Future[Boolean] =
+        asyncRedis {
+            _.sadd(key, value) match {
+                case Some(len) =>
+                    len > 0
+                case None =>
+                    logger.info(s"$key is not a set")
+                    false
+            }
+        }
+
+    // remove from set
+    def removeFromList(key: String, value: String): Future[Boolean] =
+        asyncRedis {
+            _.srem(key, value) match {
+                case Some(len) =>
+                    len > 0
+                case None =>
+                    logger.info(s"$key is not a set")
+                    false
+            }
+        }
+
 }
---
+++ src/main/scala-2.12/utils/SessionManager.scala
@@ -0,0 +1,10 @@
+package utils
+
+/**
+  * Created by rois on 11/02/2017.
+  */
+object SessionManager {
+    
+   // def registerUser(nickName: String)
+    
+}
